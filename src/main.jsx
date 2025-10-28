aec92a29f79d30ed09c04bdc4a2770f278cca202
KAYT-Techstarter <marvin.young.main@gmail.com>
2025-10-28 12:43:43 +0100

    Zwischenspeicherung
    Neu : Error Page , Profile Page
    
    Update : Header  , Image Struktur ,
    
    Entfernt : Context Ordern mit Auth.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { AuthProvider } from './contexts/AuthContext';
+import './main.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard';
@@ -9,6 +8,8 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Planer from './pages/Planer';
+import Profile from './pages/Profile';
+import Error from './pages/Error';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -18,10 +19,11 @@ createRoot(document.getElementById('root')).render(
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/planer" element={<Planer />} />
-          <Route path="*" element={<h1>404: Seite nicht gefunden</h1>} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </AuthProvider>
